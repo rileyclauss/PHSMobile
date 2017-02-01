ce83fd24c8f38600784e6d93fef367ff2a1d11ba
Micah Focht <bandmfocht@gmail.com>
2017-02-01 13:41:06 -0500

    small changes

--- PHSApp/app/src/main/java/com/phsapp/phsapp/athleticsActivity.java
+++ PHSApp/app/src/main/java/com/phsapp/phsapp/athleticsActivity.java
@@ -1,21 +1,20 @@
 package com.phsapp.phsapp;
-
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.crazyhitty.chdev.ks.rssmanager.OnRssLoadListener;
 import com.crazyhitty.chdev.ks.rssmanager.RssItem;
 import com.crazyhitty.chdev.ks.rssmanager.RssReader;
-
 import android.widget.*;
-
 import java.util.List;
 
 public class athleticsActivity extends AppCompatActivity implements OnRssLoadListener{
+    String links[] = new String[10];
+    TextView texts[] = new TextView[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_athletics);
+
         loadFeeds();
 
     }
@@ -34,6 +33,9 @@ public class athleticsActivity extends AppCompatActivity implements OnRssLoadLis
         Toast.makeText(athleticsActivity.this, rssItems.get(0).getTitle(), Toast.LENGTH_SHORT).show();
         ImageView img= (ImageView) findViewById(R.id.imageView);
         img.setImageResource(R.drawable.blacksquare);
+        texts[0] = (TextView) findViewById(R.id.textView54);
+        for(int i=0; i<10; i++){links[i] = rssItems.get(i).getLink();}
+        for(int i=0;i<10;i++){texts[i].setText(rssItems.get(i).getTitle());}
     }
 
     @Override
--- PHSApp/app/src/main/java/com/phsapp/phsapp/newsActivity.java
+++ PHSApp/app/src/main/java/com/phsapp/phsapp/newsActivity.java
@@ -1,19 +1,15 @@
 package com.phsapp.phsapp;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
-import pl.droidsonroids.gif.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
-import android.support.customtabs.CustomTabsIntent;
-import com.crazyhitty.chdev.ks.rssmanager.OnRssLoadListener;
-import com.crazyhitty.chdev.ks.rssmanager.RssItem;
 import com.crazyhitty.chdev.ks.rssmanager.RssReader;
+import android.support.customtabs.CustomTabsIntent;
+import com.crazyhitty.chdev.ks.rssmanager.*;
 import java.util.List;
-
 
 public class newsActivity extends AppCompatActivity implements OnRssLoadListener, View.OnClickListener{
      String links[] = new String[10];
@@ -24,40 +20,10 @@ public class newsActivity extends AppCompatActivity implements OnRssLoadListener
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_news);
-       loadtext = (TextView)findViewById(R.id.textView);
-       loadgif = (pl.droidsonroids.gif.GifTextView)findViewById(R.id.load);
+       loadtext = (TextView) findViewById(R.id.textView);
+       loadgif = (pl.droidsonroids.gif.GifTextView) findViewById(R.id.load);
        loadFeeds();
    }
-  /*  @Override                      TODO KEEP THIS CODE, DELETE WHEN WE'VE FINALIZED IT
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
-        final pl.droidsonroids.gif.GifTextView loading = (pl.droidsonroids.gif.GifTextView) findViewById(R.id.load);
-        final WebView myWebView = (WebView) findViewById(R.id.webview1);
-        final TextView ltext = (TextView) findViewById(R.id.textView);
-        myWebView.setVisibility(View.INVISIBLE);
-        WebSettings websettings = myWebView.getSettings();
-        websettings.setJavaScriptEnabled(true);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://penn.phmschools.org/news");
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                myWebView.loadUrl("javascript:(function() { " +
-                        "var head = document.getElementsByID('main-header')[0];"
-                        + "head.parentNode.removeChild(head);"+
-                        "})()");
-                myWebView.loadUrl("javascript:(function() { " +
-                        "var head = document.getElementsByTagName('main-footer')[0];"
-                        + "head.parentNode.removeChild(head);"+
-                        "})()");
-                loading.setVisibility(View.INVISIBLE);
-                ltext.setVisibility(View.INVISIBLE);
-                myWebView.setVisibility(View.VISIBLE);
-            }
-        }
-        );
-    }*/
   private void loadFeeds() {
         String[] urlArr = {"https://penn.phmschools.org/rss.xml"};
         new RssReader(newsActivity.this)
@@ -78,16 +44,19 @@ public class newsActivity extends AppCompatActivity implements OnRssLoadListener
         texts[7] = (TextView) findViewById(R.id.textView9);
         texts[8] = (TextView) findViewById(R.id.textView10);
         texts[9] = (TextView) findViewById(R.id.textView11);
+        TextView text55 = (TextView) findViewById(R.id.textView55);
+        String newVal = rssItems.get(0).getDescription().substring(0,70);
+        text55.setText(newVal);
         for(int i=0; i<10; i++){links[i] = rssItems.get(i).getLink();}
         for(int i=0;i<10;i++){texts[i].setText(rssItems.get(i).getTitle());}
     }
     @Override
     public void onClick(View view) {
-            String url = "https://paul.kinlan.me/";
+      /*      String url = "https://paul.kinlan.me/";
             CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
             CustomTabsIntent customTabsIntent = builder.build();
             customTabsIntent.launchUrl((Activity) getApplicationContext(), Uri.parse(url));
-            Toast.makeText(newsActivity.this, "This is a test message", Toast.LENGTH_SHORT).show();
+            Toast.makeText(newsActivity.this, "This is a test message", Toast.LENGTH_SHORT).show();*/
     }
     public void onFailure(String message) {
         Toast.makeText(newsActivity.this, "Error: "+message, Toast.LENGTH_SHORT).show();
