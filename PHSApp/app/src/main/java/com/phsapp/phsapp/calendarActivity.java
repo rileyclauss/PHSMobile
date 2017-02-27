91a1e786aef4f6e88954e1b45b0869047ee6176e
Micah Focht <bandmfocht@gmail.com>
2017-02-27 13:43:36 -0500

    Calendar and News
    Integrated Calendar and fixed News activity to open

--- PHSApp/app/src/main/java/com/phsapp/phsapp/calendarActivity.java
+++ PHSApp/app/src/main/java/com/phsapp/phsapp/calendarActivity.java
@@ -1,16 +1,19 @@
 package com.phsapp.phsapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import com.prolificinteractive.materialcalendarview.*;
 
 public class calendarActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        MaterialCalendarView mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
 
+        mcv.setShowOtherDates(MaterialCalendarView.SHOW_NONE);
     }
 
 }
--- PHSApp/app/src/main/java/com/phsapp/phsapp/newsActivity.java
+++ PHSApp/app/src/main/java/com/phsapp/phsapp/newsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import android.widget.TextView;
 import com.crazyhitty.chdev.ks.rssmanager.RssReader;
 import com.crazyhitty.chdev.ks.rssmanager.*;
 import java.io.InputStream;
@@ -38,27 +39,26 @@ public class newsActivity extends AppCompatActivity implements OnRssLoadListener
     public void onSuccess(List<RssItem> rssItems) {
         loadgif.setVisibility(View.INVISIBLE);
         loadtext.setVisibility(View.INVISIBLE);
-        //views[0] = (ImageView) findViewById(R.id.imageView3);
         texts[0] = (TextView) findViewById(R.id.textView2);
-        texts[1] = (TextView) findViewById(R.id.textView3);
-        texts[2] = (TextView) findViewById(R.id.textView4);
-        texts[3] = (TextView) findViewById(R.id.textView5);
+        texts[1] = (TextView) findViewById(R.id.textView56);
+        texts[2] = (TextView) findViewById(R.id.textView57);
+       /* texts[3] = (TextView) findViewById(R.id.textView5);
         texts[4] = (TextView) findViewById(R.id.textView6);
         texts[5] = (TextView) findViewById(R.id.textView7);
         texts[6] = (TextView) findViewById(R.id.textView8);
         texts[7] = (TextView) findViewById(R.id.textView9);
         texts[8] = (TextView) findViewById(R.id.textView10);
-        texts[9] = (TextView) findViewById(R.id.textView11);
+        texts[9] = (TextView) findViewById(R.id.textView11);*/
         int x, y;
-        for(int i=0;i<10;i++){
+        for(int i=0;i<2;i++){
             x = rssItems.get(i).getDescription().indexOf(" width=");
             x -= 1;
             y = rssItems.get(i).getDescription().indexOf("article_image");
             y -=67;
             imgLinks[i] = rssItems.get(i).getDescription().substring(y, x);
         }
-      //  for(int i=0; i<10; i++){links[i] = rssItems.get(i).getLink();}
-      //  for(int i=0;i<10;i++){texts[i].setText(rssItems.get(i).getTitle());}
+        for(int i=0; i<10; i++){links[i] = rssItems.get(i).getLink();}
+        texts[0].setText(rssItems.get(0).getTitle());
     }
     @Override
     public void onClick(View view) {
