5bbbc3bc4d087e7f559d7d10667a0ec39218bc8a
Micah Focht <bandmfocht@gmail.com>
2017-04-21 13:36:35 -0400

    small update

--- PHSApp/app/src/main/java/com/phsapp/phsapp/aboutActivity.java
+++ PHSApp/app/src/main/java/com/phsapp/phsapp/aboutActivity.java
@@ -2,16 +2,12 @@ package com.phsapp.phsapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 public class aboutActivity extends AppCompatActivity {
-int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast.makeText(aboutActivity.this, "it loads again", Toast.LENGTH_SHORT ).show();
-        if (i==0){
-                super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);}
-        i++;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
     }
 }
