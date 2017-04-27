6c9fd3759c9a3e8ffb0ed773a70ebd59d2072ab4
Micah Focht <bandmfocht@gmail.com>
2017-04-27 11:45:10 -0400

    Revert "Work on News Activity"
    This reverts commit a90770ab55ff6731c37a0ab9c9995c00fa23bff4.
    
    # Conflicts:
    #	PHSApp/app/src/main/java/com/phsapp/phsapp/newsActivity.java

--- PHSApp/app/src/main/java/com/phsapp/phsapp/MainActivity.java
+++ PHSApp/app/src/main/java/com/phsapp/phsapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.penntheme);
+        setTheme(R.style.NewPennTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTheme(R.style.backgroundtransparent);
